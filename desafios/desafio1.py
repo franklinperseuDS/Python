1a7d53c99c3f1a799a8d2d33d4159082af629384
franklinperseuDS <69854802+franklinperseuDS@users.noreply.github.com>
2021-02-05 12:54:12 -0400

    primeiro exercicio
    usando um simples input e fazendo um print

---
+++ desafios/desafio1.py
@@ -0,0 +1,6 @@
+#crie um script que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+
+nome = input("Digite seu nome:\n")
+
+print ("Seja bem-vindo, ",nome)
