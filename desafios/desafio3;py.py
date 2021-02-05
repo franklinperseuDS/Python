bc51b1520ca0dd69ae526026a95dc0908cbc2a54
franklinperseuDS <69854802+franklinperseuDS@users.noreply.github.com>
2021-02-05 14:14:46 -0400

    Create desafio3;py.py

---
+++ desafios/desafio3;py.py
@@ -0,0 +1,6 @@
+#Adicionar dois números e mostrar a soma
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+
+print(numero1,"+",numero2,"=",numero1+numero2)
