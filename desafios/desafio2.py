06840701ae530eea09dcfe2708a75a4ecb2fd9ed
franklinperseuDS <69854802+franklinperseuDS@users.noreply.github.com>
2021-02-05 14:12:13 -0400

    Create desafio2.py

---
+++ desafios/desafio2.py
@@ -0,0 +1,7 @@
+#Criar um script que leia o dia, o mês e o ano de nascimento de uma pessoa e mostrar uma mensagem com a data formatada
+
+dia = int(input("Digite o dia do seu nascimento: "))
+mes = input("Digite o mês do seu nascimento: ")
+ano = int(input("Digite o ano do seu nascimento: "))
+
+print("Você nasceu no dia",dia,"de",mes,"de",ano,". Correto?")
