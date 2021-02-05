c2bc78ca612a26865cbf15320a0dc0d06303f2cc
franklinperseuDS <69854802+franklinperseuDS@users.noreply.github.com>
2021-02-05 17:22:26 -0400

    Update exer004.py

--- exercicios/exer004.py
+++ exercicios/exer004.py
@@ -1,8 +1,8 @@
 #Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 print("==========Exercicio 4 ==========")
-n = input("digite alguma coisa: ")
+n = input("Digite alguma coisa: ")
 
-print(type(n))
+print("O tipo primitivo é: ",type(n))
 
 print("É um alpha númerico ?", n.isalnum())
 print("É um alfabético ?",n.isalpha())
