c531d0d1913084caf99e1c1d515ed23ea0d1f0ef
franklinperseuDS <69854802+franklinperseuDS@users.noreply.github.com>
2021-02-11 09:38:25 -0400

    Update exer021.py

--- exercicios/exer021.py
+++ exercicios/exer021.py
@@ -1,2 +1,9 @@
 # Faça um programa que abra e reporduza o áudio de um arquivo MP3
 
+import pygame
+pygame.init()
+print('tocando musica')
+pygame.mixer.music.load('exe021.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
